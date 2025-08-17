425c5cbd060c59ed19b1a06aa418f95375e2a84e
sirsly <mariocardona0501@gmail.com>
2025-08-17 17:25:14 -0600

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mypkg",
+    name="mypkg-janiramimariquesi",
     version="0.1.0",
+    author="Mario Cristales",
+    description="Demo de paquete para taller GitHub Actions",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     packages=find_packages(),
+    python_requires=">=3.8",
 )
